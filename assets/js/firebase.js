6c8f05ec2ed93f97fbafa8869b501a89a8a6aa71
sachinmeena1994 <skillsuup@gmail.com>
2024-10-01 23:21:38 +0530

    added information collection and other stulf

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -5,6 +5,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebas
 
 
 let userData=[]
+let establishment;
   // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyCauitOiKDUc0iI0rYZrl5fcJ_Wnth_Zp4",
@@ -38,5 +39,31 @@ let userData=[]
   }
 
 
+    // Function to fetch users from the 'information' collection
+    async function fetchUEstablishment() {
+        try {
+          const usersCollection = collection(db, 'information'); // Get a reference to the 'users' collection
+          const userSnapshot = await getDocs(usersCollection); // Fetch all documents in the collection
+          const userList = userSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); // Map documents to an array of objects
+    
+          establishment=[...userList][0]
+          console.log(establishment)
+          document.getElementById("estabishmentInformation").innerHTML=establishment.establishment;
+          document.getElementById("point1").innerHTML+=establishment.point1
+          document.getElementById("point2").innerHTML+=establishment.point2
+          document.getElementById("point3").innerHTML+=establishment.point3
+          document.getElementById("point4").innerHTML+=establishment.point4
+          document.getElementById("point5").innerHTML+=establishment.point5
+          document.getElementById("point6").innerHTML+=establishment.point6
+          document.getElementById("point7").innerHTML+=establishment.point7
+          document.getElementById("point8").innerHTML+=establishment.point8
+        } catch (error) {
+          console.error("Error fetching user data: ", error);
+        }
+      }
+
+
+
   // Call the function to fetch users
   fetchUsers();
+  fetchUEstablishment();
