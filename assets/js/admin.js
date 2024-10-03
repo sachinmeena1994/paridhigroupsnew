9b12e10319977c3c05c7ab39f49e07438aeb5779
“Praveen” <Praveen.badam781@gmail.com>
2024-10-03 13:34:13 +0530

    added changes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,8 @@
-setTimeout(() => {
-  const userData = localStorage.getItem("userData");
-  console.log(userData);
-}, 3000);
+document.getElementById("loginbtn").addEventListener("click",() => {
+  const emailId = document.getElementById("adminEmail").value
+  const PasswordId = document.getElementById("adminPassword").value
+  console.log(emailId,PasswordId)
+  if (userData[0].email == emailId && userData[0].password == PasswordId) {
+    document.getElementById("parentContainer").style.display = "none"
+   }
+})
--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -40,14 +40,7 @@ let establishment;
   }
 
 
-document.getElementById("loginbtn").addEventListener("click",() => {
-  const emailId = document.getElementById("adminEmail").value
-  const PasswordId = document.getElementById("adminPassword").value
-  console.log(emailId,PasswordId)
-  if (userData[0].email == emailId && userData[0].password == PasswordId) {
-    document.getElementById("parentContainer").style.display = "none"
-   }
-})
+
 // document.getElementById("uploadPropertyBtn").addEventListener("click",() => {
 //   const TitleProperty = document.getElementById("propertyTitle").value
 //   const DescriptionProperty = document.getElementById("propertyDescription").value
